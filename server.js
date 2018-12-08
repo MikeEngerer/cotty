dae82c1dbedcdb4470aaf50ac5104613fd635204
MikeEngerer <michael.engerer@hotmail.com>
2018-12-08 18:36:34 +0000

    app.post / for nights change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
-let { numNights, costCotty, costFood } = require("./data/data")
-let pricePerPerson = require("./scripts/price_per_person").pricePerPerson;
+let { numNights, paymentStatus, costCotty, costFood } = require("./data/data");
+let { pricePerPerson }= require("./scripts/price_per_person");
 
 let PORT = 8080;
 const app = express();
@@ -16,7 +16,11 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-	res.redirect("/")
+	let personInDB = Object.keys(req.body)
+	let oldNights = numNights[personInDB[1]];
+	let newNights = req.body.nights
+	numNights[personInDB[1]] = Number(newNights)
+	res.redirect("/");
 });
 
 app.listen(PORT, function() {
